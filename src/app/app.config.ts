ad2a1fbd1d327e198b5b5f6256b37be68be8379e
Eswar Damarlapati <Eswar.damarlapati@gmail.com>
2026-01-10 11:28:27 +0530

    removed localize package

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,13 +1,9 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners, isDevMode } from '@angular/core';
 import { provideRouter, withInMemoryScrolling } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
-import { registerLocaleData } from '@angular/common';
-import localeEn from '@angular/common/locales/en';
 
 import { routes } from './app.routes';
 import { provideServiceWorker } from '@angular/service-worker';
-
-registerLocaleData(localeEn);
 
 export const appConfig: ApplicationConfig = {
   providers: [
